6d11c29c11929e1487755d1654842ac79f4fdaeb
Meng Lu <lumeng.dev@gmail.com>
2013-10-28 23:28:14 -0500

    +added script file-system/add-filename-extension-to-image-files.sh

---
+++ file-system/add-filename-extension-to-image-files.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+## Summary: identify image file type and add filename extension appropriately when applicable
+## Example:
+## $ add-filename-extension-to-image-files.sh -d ~/temp
+##
+
+IMAGE_FILE_DIR=
+
+while getopts d:d opt; do
+  case $opt in
+  d)
+      IMAGE_FILE_DIR=$OPTARG
+      ;;
+  esac
+done
+
+#echo "IMAGE_FILE_DIR="$IMAGE_FILE_DIR
+
+#IMAGE_FILE_DIR=/Users/lumeng/Dropbox-x4430/Dropbox/DataSpace-Dropbox/Image/Wallpaper/kuvva_wallpaper/new/
+
+
+
+if [[ -d $IMAGE_FILE_DIR ]]; then
+	FILES=`find $IMAGE_FILE_DIR -maxdepth 1 -type f`
+else
+	FILES=()
+fi
+
+
+IMAGEMAGICK_IDENTIFY_BIN="/opt/local/bin/identify"
+
+
+for file in $FILES
+do
+	filename=$(basename "$file")
+    extension="${filename##*.}"
+	if [ "$extension" == "$filename" ]; then
+		newextension=`$IMAGEMAGICK_IDENTIFY_BIN $file | cut -d ' '  -f 2 | tr '[:upper:]' '[:lower:]'`
+		rsync -qrthp $file "$file.$newextension"
+		rm $file
+	fi
+done
+
+## END
