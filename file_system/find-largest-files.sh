817cf949bdf24589ca7a7902edc89c72c3563e02
Meng Lu <lumeng.dev@gmail.com>
2014-03-19 17:51:54 -0700

    + fix the previously near empty file

--- file_system/find-largest-files.sh
+++ file_system/find-largest-files.sh
@@ -2,4 +2,12 @@
 
 ## Summary: find largest files recursively
 
-du -h
+TIMESTANP=`time +%s`
+OUTPUTFILE="/tmp/list_of_large_files__$TIMESTAMP.txt"
+
+find -P -O2 . -type f -size +100M -user $USER -exec ls -sh '{}' >> $OUTPUTFILE \;
+
+## A different method
+#du -h | awk '{printf "%s %08.2f\t%s\n", index("KMG", substr($1, length($1))), substr($1, 0, length($1)-1), $0}'
+
+## EOF
