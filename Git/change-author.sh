3df5dbdebc99ede8597833bd55772c78c597964b
Meng Lu <lumeng.dev@gmail.com>
2014-03-25 20:51:33 -0700

    better paramter setting

--- Git/change-author.sh
+++ Git/change-author.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
- 
-git filter-branch --env-filter '
- 
+
+## Summary: change identification (user name and email) in Git revision history
+
+## Edit the following to specify what old identity you'd like to modify
+export OLD_EMAIL="old_user@host.com"
+
+
+## Edit the following to specify what the new user name and email address you want
+export NEW_NAME="New User Name"
+export NEW_EMAIL="new_user@host.com"
+
+git filter-branch -f --env-filter '
+
 an="$GIT_AUTHOR_NAME"
 am="$GIT_AUTHOR_EMAIL"
 cn="$GIT_COMMITTER_NAME"
 cm="$GIT_COMMITTER_EMAIL"
  
-if [ "$GIT_COMMITTER_EMAIL" = "lumeng.dev@gmail.com" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    cn="MathCraft"
-    cm="mathcraft.org@gmail.com"
+    cn="$NEW_NAME"
+    cm="$NEW_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "lumeng.dev@gmail.com" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    an="MathCraft"
-    am="mathcraft.org@gmail.com"
+    an="$NEW_NAME"
+    am="$NEW_EMAIL"
 fi
  
 export GIT_AUTHOR_NAME="$an"
