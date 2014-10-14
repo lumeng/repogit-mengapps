660fea9e68f534bf578b1d783e47045a635d8d8d
Meng Lu <lumeng.dev@gmail.com>
2014-10-14 12:55:14 -0700

    + Add example usage.
    Signed-off-by: Meng Lu <lumeng.dev@gmail.com>

--- file_system/tally_file_name_extension.sh
+++ file_system/tally_file_name_extension.sh
@@ -4,11 +4,19 @@
 ##
 ## ToDo:
 ## * test against different versions of grep
+##
+## Example:
+##     $ App/file_system/tally_file_name_extension.sh
+##     1851 .m
+##       92 .bak
+##        5 .~
+##        1 .Log
+##
 
 DIR=${1:-`pwd`}
 
 find ${DIR} -type f -exec basename {} \; `# find all files and extract their file names` \
-	| grep -o -e '\.[^.]\+$' `# find ` \
-	| sort \
-	| uniq -c `#` \
-	| sort -r # sort according to occurrences
+    | grep -o -e '\.[^.]\+$' `# find ` \
+    | sort \
+    | uniq -c `#` \
+    | sort -r # sort according to occurrences
