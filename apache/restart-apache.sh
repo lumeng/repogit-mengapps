694f37f86eebdf663c6a42fc59c0ce8cb962e5a1
Meng Lu <lumeng.dev@gmail.com>
2015-02-10 14:52:04 -0800

    Add a script to restart Apache.

---
+++ apache/restart-apache.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+## Summary: Start Apache2 server
+#+ Author: Meng Lu <meng@woflram.com>
+#+ Date: 201502
+#+
+#+ ## References:
+#+ * http://httpd.apache.org/docs/2.0/stopping.html
+##
+
+if [ `uname -s` == 'Darwin' &&  -e /usr/local/apache2/bin/apachectl ]; then
+    sudo /usr/local/apache2/bin/apachectl -k restart
+elif [ -e /usr/sbin/apachectl ]; then
+	sudo /usr/sbin/apachectl -k restart
+else
+	exit 1
+fi
+
+## END
