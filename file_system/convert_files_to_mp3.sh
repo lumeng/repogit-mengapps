567225ef44a71d10284eb51a3a948f950db298da
Meng Lu <lumeng.dev@gmail.com>
2016-01-05 16:23:33 -0800

    + Add script to convert files to MP3.

---
+++ file_system/convert_files_to_mp3.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+FILES=./*
+
+type ffmpeg >/dev/null 2>&1 || { echo >&2 "This script requires ffmpeg but it's not installed.  Aborting."; exit 1; }
+
+shopt -s nocasematch
+
+for f in $FILES
+do
+	echo "Processing $f file ..."
+	full_file_name=$(basename "$f")
+	file_name_extension="${full_file_name##*.}"
+	file_name="${full_file_name%.*}"
+	if [[ ! $file_name_extension =~ "mp3" && ! -e "$file_name.mp3" ]]; then
+		ffmpeg -i "$f" "$file_name.mp3"
+	fi
+done
+
+
