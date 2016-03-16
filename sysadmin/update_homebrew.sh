42659fca7231600a8a7b8771568aa2897c4f45db
Meng Lu <lumeng.dev@gmail.com>
2016-03-15 22:52:12 -0700

    % Only include necessary paths into $PATH before running brew to update to reduce chances of conflicts.

--- sysadmin/update_homebrew.sh
+++ sysadmin/update_homebrew.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+##
+#+ Script for updating Homebrew.
+#+
+##
+
+## Abort if not on Mac OS X.
+if [[ ! $(uname) == 'Darwin' ]]; then
+    echo >&2 "Since Homebrew is a package management system for Mac OS X, it does not make sense to update it on $(uname). Abort."
+	exit 1
+fi
+
+
 ## If brew is not installed, exit.
+export PATH=/usr/local/bin:$PATH  ## Make sure brew is on the path before checking its existence.
 type brew >/dev/null 2>&1 || { echo >&2 "Homebrew is not installed. Aborting."; exit 1; }
+
 
 ## Save old $PATH and restore it later
 OLD_PATH="$PATH"
@@ -9,13 +23,16 @@ OLD_PATH="$PATH"
 ## Get the path of binaries installed via Homebrew.
 BREW_BIN_PATH=$(brew --prefix)/bin
 
+## The most basic paths that should be included in $PATH
+BASIC_PATH=/Applications/Xcode.app/Contents/Developer/usr/bin:/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/sbin
+
 ##
 #+ Prepend Homebrew binary path to $PATH, so when installing of compiling
 #+ softwares using brew, it sees softwares and libraries installed by
 #+ brew first.
 ##
 
-export PATH=$BREW_BIN_PATH:$PATH
+export PATH=$BREW_BIN_PATH:$BASIC_PATH
 
 brew update
 brew upgrade
