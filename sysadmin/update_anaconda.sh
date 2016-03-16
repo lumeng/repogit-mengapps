f5fa5f84edfaae25956eb25d9477e9af879ed54c
Meng Lu <lumeng.dev@gmail.com>
2016-03-15 22:51:22 -0700

    % Only include necessary paths into $PATH before running conda to update to reduce chances of conflicts.

--- sysadmin/update_anaconda.sh
+++ sysadmin/update_anaconda.sh
@@ -13,12 +13,25 @@ else
 	exit 1
 fi
 
+
+## The most basic paths that should be included in $PATH
+if [[ $(uname) == 'Linux' ]]; then
+    BASIC_PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/games
+elif [[ $(uname) == 'Darwin' ]]; then
+    BASIC_PATH=/Applications/Xcode.app/Contents/Developer/usr/bin:/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/sbin
+else
+    echo >&2 "Behavior on OSs other than Mac OS X and Ubuntu Linux is not assured."
+	BASIC_PATH=$PATH
+fi
+
+
+
 ##
 #+ Prepend Anaconda binary path to $PATH, so when installing of compiling
 #+ softwares using conda, it sees softwares and libraries installed by
 #+ conda first.
 ##
-export PATH=$ANACONDA_BIN_PATH:$PATH
+export PATH=$ANACONDA_BIN_PATH:$BASIC_PATH
 
 ## If brew is not installed, exit.
 type conda >/dev/null 2>&1 || { echo >&2 "Anaconda is not installed. Aborting."; exit 1; }
