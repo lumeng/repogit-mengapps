a9f55c0e13f40099b407aff24a842377adc50a79
Meng Lu <lumeng.dev@gmail.com>
2017-03-07 19:54:02 -0800

    Add wolfram-notebook-diff/ subdirectory that contains wolfram-notebook-diff.wls, a Wolfram Language script to diff two Wolfram Notebook files with AuthorTools`NotebookDiff function, and a Bash script wolfram-notebook-diff.sh, which is a wrapper to the first script so the diff result can be open by the Mathematica front-end. Add a git-difftool-wrapper.sh that can be used to hook up wolfram-notebook-diff.sh to the "git diff" command such that Wolfram Notebook files, i.e. *.nb files are diff-ed with wolfram-notebook-diff.sh while other files are diff-ed with the other diff-ing software.

---
+++ git-tool/wolfram-notebook-diff/git-difftool-wrapper.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+##
+## to-do:
+##
+## Mathematica .nb notebooks should be diffed with special tool.
+##
+
+# diff is called by git with 7 parameters:
+# path old-file old-hex old-mode new-file new-hex new-mode
+
+OLDFILE=$2
+NEWFILE=$5
+
+old_file_name=$(basename "$OLDFILE")
+old_file_name_extension="${old_file_name##*.}"
+old_file_name_base="${old_file_name%.*}"
+
+new_file_name=$(basename "$NEWFILE")
+new_file_name_extension="${new_file_name##*.}"
+new_file_name_base="${new_file_name%.*}"
+
+## Ignore lettercase for regex match.
+shopt -s nocasematch
+
+if [[ "$old_file_name_extension" =~ nb|cdf && "$new_file_name_extension" =~ nb|cdf ]]; then
+	echo "diff-ing Wolfram notebooks:
+$2
+$5
+"
+    wolfram-notebook-diff "$2" "$5"
+	exit 0
+else
+# /Developer/Applications/Utilities/FileMerge.app
+#
+# opendiff is same as FileMerge
+# "/usr/bin/opendiff" "$2" "$5" | cat
+
+##
+#+ Install from https://sourcegear.com/diffmerge/
+##
+#wolfram-notebook-diff "$2" "$5"
+    diffmerge "$2" "$5"
+fi
+
+## END
