494209165627521727483276a83ce24e239c0118
Meng Lu <lumeng.dev@gmail.com>
2017-07-20 16:45:41 -0700

    + Add script for updating TeX softwares: update_tex.sh.

---
+++ sysadmin/update_tex.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+##
+#+ Script for updating LaTeX and packages.
+#+
+##
+
+## Save old $PATH and restore it later
+OLD_PATH="$PATH"
+
+## The most basic paths that should be included in $PATH
+BASIC_PATH=/Applications/Xcode.app/Contents/Developer/usr/bin:/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/sbin
+
+##
+#+ Prepend Homebrew binary path to $PATH, so when installing of compiling
+#+ softwares using brew, it sees softwares and libraries installed by
+#+ brew first.
+##
+
+export PATH=/Library/TeX/texbin/:${BASIC_PATH}
+
+export TEXMFHOME=$(kpsewhich -var-value=TEXMFHOME)
+
+if [[ ! -d $TEXMFHOME ]]; then
+	mkdir $TEXMFHOME
+fi
+
+##
+#+ Update 
+#+
+#+
+
+#+ References:
+#+ *
+
+tlmgr update --self
+
+
+################################################################################
+#+ Install softwares.
+##
+
+## 2017-7-17： Install shading package.
+curl --create-dirs -o $TEXMFHOME/shading.sty http://ctan.mirrors.hoobly.com/macros/latex209/contrib/shading/shading.sty
+
+curl --create-dirs -o $TEXMFHOME/shading.tex http://ctan.mirrors.hoobly.com/macros/latex209/contrib/shading/shading.tex
+
+#tlmgr install shading
+
+
+## more ...
+
+
+## Restore $PATH.
+export PATH=$OLD_PATH
+
+## END
