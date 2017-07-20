4491826b04c0ba296ed5ee3bc2aedf9393e5433a
Meng Lu <lumeng.dev@gmail.com>
2017-07-19 22:51:50 -0700

    % Update file path to Git repo "git". Run "git pull" from it before other update commands.

--- sysadmin/update_git-mediawiki.sh
+++ sysadmin/update_git-mediawiki.sh
@@ -24,16 +24,18 @@ fi
 ## This arbitrarily assumes git://git.kernel.org/pub/scm/git/git.git
 #+ is checked out at the following workspace directory.
 ##
-WORKSPACE_DIR="$HOME/WorkSpace-$HOST_NAME"
+WORKSPACE_DIR="$HOME/WorkSpace-$HOST_NAME/repos-external"
 
 if [[ -d "$WORKSPACE_DIR" ]]; then
 	cd "$WORKSPACE_DIR"
+	cd git
+	git pull
 fi
 	
 if [[ -f "$WORKSPACE_DIR/git/.git/config" ]]; then
-	sudo cp git/contrib/mw-to-git/git-remote-mediawiki.perl "$(git --exec-path)/git-remote-mediawiki"
+	sudo cp contrib/mw-to-git/git-remote-mediawiki.perl "$(git --exec-path)/git-remote-mediawiki"
 	sudo chmod a+x "$(git --exec-path)/git-remote-mediawiki"
-	sudo cp git/contrib/mw-to-git/git-mw.perl "$(git --exec-path)/git-mw"
+	sudo cp contrib/mw-to-git/git-mw.perl "$(git --exec-path)/git-mw"
 	sudo chmod a+x "$(git --exec-path)/git-mw"
 else
     >&2 echo "This script currently assumes the Git repository git://git.kernel.org/pub/scm/git/git.git is cloned to $WORKSPACE_DIR/git but it is not a valid Git repository (.git/config does not exist.)"
