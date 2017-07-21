d32f3d7751f4f30ddbdad02a9396abbde1a752ed
Meng Lu <lumeng.dev@gmail.com>
2017-07-20 22:46:04 -0700

    ~ Untabify and clean up whitespaces.

--- file_system/compress-file-bzip2-gzip.sh
+++ file_system/compress-file-bzip2-gzip.sh
@@ -6,17 +6,33 @@
 #+ Author: Meng Lu <lumeng.dev@gmail.com>
 #+
 #+ Usage:
-#+ 
+#+     compress-file-bzip2-gzip.sh myfile
+#+
+#+ * View gzip file on the fly:
+#+
+#+     zless data.txt.gz
+#+     zmore data.txt.gz
+#+
+#+ More information about z* commands:
+#+ http://www.cyberciti.biz/tips/decompress-and-expand-text-files.html
+#+
+#+ * View bzip2 file on the fly:
+#+ Copy the following script to bzless.sh
+#+ http://www.bzip.org/bzip2-howto/with-less.html
+#+
+#+     bzless myfile.bzip2
+#+
 ##
 
 FILE=$1
 
 if [[ ! -f "$FILE" ]]; then
-	echo >&2 '$FILE is not a regular file. Abort.'
-	exit 1
+    echo >&2 '$FILE is not a regular file. Abort.'
+    exit 1
 fi
-   
-{ type bzip3 >/dev/null 2>&1 && bzip3 -qz "$FILE" >/dev/null; } || \
-    gzip -c "$FILE" > "$LOG_FILE.gz"
+
+{ type bzip2 >/dev/null 2>&1 && \
+      bzip2 -qz "$FILE" >/dev/null; } || \
+	gzip -c "$FILE" > "$LOG_FILE.gz"
 
 ## END
