c4a0c04aa9248f871cca717e6a5ee21f6bd1a4d4
Meng Lu <lumeng.dev@gmail.com>
2021-01-01 15:49:01 -0800

    % Accommodate local hostname ending in ".lan"; Export retrieved local host name to variable $meng_HOST_NAME.

--- sysadmin/get_simple_host_name.sh
+++ sysadmin/get_simple_host_name.sh
@@ -5,20 +5,21 @@
 #+ Author: Meng Lu <lumeng.dev@gmail.com>
 ##
 
-REGEX='(.*)\.local'
+HOST_NAME_REGEX='(.*)(\.local|\.lan)'
 
 SIMPLE_HOST_NAME="$(hostname)"
 
-# echo "[DEBUG] ${SIMPLE_HOST_NAME}"
-
-HOST_NAME_REGEX='(.*)\.local'
+echo "[DEBUG] ${SIMPLE_HOST_NAME}"
 
 if [[ "${SIMPLE_HOST_NAME}" =~ ${HOST_NAME_REGEX} ]]; then
-    #echo "[DEBUG] matched!"
-    echo "${BASH_REMATCH[1]}"
+#    echo "[DEBUG] matched!"
+#    echo "[DEBUG] ${BASH_REMATCH[1]}"
+    export meng_HOST_NAME="${BASH_REMATCH[1]}"
 else
-	:
-    #echo "[DEBUG] does not match!"	
+#    echo "[DEBUG] does not match!"	
+    export meng_HOST_NAME="${SIMPLE_HOST_NAME}"
 fi
+
+# echo "[DEBUG] $meng_HOST_NAME"
 
 ## END
