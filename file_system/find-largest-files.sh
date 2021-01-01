2712566fb50359476800d447bfcd45265ed40218
Meng Lu <lumeng.dev@gmail.com>
2021-01-01 15:46:11 -0800

    % Use Gnu find instead of system find.

--- file_system/find-largest-files.sh
+++ file_system/find-largest-files.sh
@@ -8,7 +8,7 @@ TIMESTAMP="$(date +%s)"
 OUTPUTFILE="${TMPSUBDIR}/list_of_large_files__${TIMESTAMP}.txt"
 touch $OUTPUTFILE
 
-find -P -O2 . -type f -size +500M -user $USER -exec ls -s '{}' \; | \
+gfind -P -O2 . -type f -size +100M -user $USER -exec ls -pks '{}' \; | \
     sort -nr -t' ' -k1,1 >> $OUTPUTFILE
 
 echo ${OUTPUTFILE}
