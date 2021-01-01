b3c0fc8ff2b6e1519835b966ef288b06dfcc9b5e
Meng Lu <lumeng.dev@gmail.com>
2021-01-01 15:45:25 -0800

    + Add more hard drives for books.

--- sysadmin/find_book.sh
+++ sysadmin/find_book.sh
@@ -4,7 +4,7 @@ type find >/dev/null 2>&1 || { echo >&2 "This script requires command-line execu
 
 BOOK_FILENAME=$1
 
-find {/Volumes/{library2,LIBRARY3},~/DataSpace-Library} -iname '$*{BOOK_FILENAME}*'
+find {/Volumes/{library2,LIBRARY3,library1,library4,library5,library6},~/DataSpace-Library,~/Downloads,~/百度云同步盘} -iname "*${BOOK_FILENAME}*"
 
 # END
 
